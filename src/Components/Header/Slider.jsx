037c682c8e682f00f624819adcd27a03be478b0c
GerryKp <gerardferre.alu@iespacomolla.es>
2023-05-13 23:23:20 +0200

    final changes to first prod merge

--- src/Components/Header/Slider.jsx
+++ src/Components/Header/Slider.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import img from '../../assets/image1.jpg'
+import me from '../../assets/image1.jpg'
+import me2 from '../../assets/image2.jpg'
+import me3 from '../../assets/image3.jpg'
+import me4 from '../../assets/image4.jpg'
+import { Image } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
+
 const Slider = () => {
 
   return (
     <>
-      <img src={img} alt="Sample Text" />
+      <Carousel slideSize="80%"  height="100%" maw={720} mx="auto" slideGap="sm" controlsOffset="md" loop dragFree withIndicators>
+        <Carousel.Slide> <Image src={me} /> </Carousel.Slide>
+        <Carousel.Slide> <Image src={me2} /> </Carousel.Slide>
+        <Carousel.Slide> <Image src={me3} /> </Carousel.Slide>
+        <Carousel.Slide> <Image src={me4} /> </Carousel.Slide>
+      </Carousel>
     </>
   )
 }
