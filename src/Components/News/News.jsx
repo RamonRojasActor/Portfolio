ccb7e25ad184ab0dd55018f28cd3f3e2a8d80ce0
GerryKp <gerardferre.alu@iespacomolla.es>
2023-05-14 21:32:56 +0200

    solver img error

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../Styles/News.css'
+import masterclass from '../../assets/masterclass.jpg'
 import NewsItem from './NewsItem'
 const News = () => {
 
@@ -14,7 +15,7 @@ const News = () => {
           txt = "El actor villenense Ramón Rojas, protagoniza una comedia de Shakespeare, que se estrena en Alicante"
         />
         <NewsItem 
-          img="https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/318516567_1643709649365704_7271166949383224594_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=973b4a&_nc_ohc=sR00gm1H8n8AX8uDDJ3&_nc_ht=scontent-mad1-1.xx&oh=00_AfDQ8rEgRfVgslcIZ2ZVpo6gWEr9MCHmN47fq3_f_zAVGw&oe=645AC7C3"
+          img={masterclass}
           href="https://www.facebook.com/losjuglaresdellute/posts/pfbid0oAd1ZXLXFM6RBAEDLT2ys4b2r11mjwauGzzNBtthac6xymZm9npoGQeHi6smap1Sl"
           title = "Masterclass con Ramón Rojas"
           txt = "Másterclass, con un paisano Ramón Rojas. Sus técnicas y amor a este arte nos ha bañado de dentro hasta fuera. Impresionante y diferente, de lo poco que conocemos...hasta los huesos."
